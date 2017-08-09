da9c12d86d1bd14e64ad470a878a5d57ece6dbe7
Philipp Wruck <alpine@quiez.io>
2017-08-09 12:09:02 +0200

    blog post added

--- layouts/main.js
+++ layouts/main.js
@@ -1,10 +1,10 @@
 import Meta from '../components/meta'
-import Link from 'next/prefetch'
+import Link from 'next/link'
 
 export default ({ children }) => (
   <div className="main">
     <div className="logo">
-      <Link href="/"><a>quiez.io</a></Link>
+      <Link prefetch href="/"><a>quiez.io</a></Link>
       {' '}
     </div>
 
--- pages/2017/writing-the-documentation-of-past-three-years.js
+++ pages/2017/writing-the-documentation-of-past-three-years.js
@@ -8,9 +8,9 @@ import withViews from '../../lib/with-views'
 
 export default withViews(({ views }) => (
   <Post>
-    <Head><title>Example Blog Post</title></Head>
-    <Title>Example Blog Post</Title>
-    <Meta date="August 04, 2017" views={ views } />
+    <Head><title>Writing the documentation for three years in one month</title></Head>
+    <Title>Writing the documentation for three years in one month</Title>
+    <Meta date="August 07, 2017" views={ views } />
     <P>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque minus soluta accusantium necessitatibus. Eaque possimus molestias repudiandae facilis omnis voluptatem inventore nemo natus, ipsum eveniet blanditiis ea, tenetur odio ex.</P>
     <P>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem aspernatur totam, suscipit fugit quo molestiae tempora! Dolore saepe, perspiciatis culpa officia consequatur repellat, doloribus sint laborum vero distinctio accusantium inventore!</P>
     <Quote by='me'>Lol</Quote>
